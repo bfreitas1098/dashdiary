fcf9a41a594c9edf1df2c5a35cdc8f6729961a28
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2024-02-24 22:14:20 -0500

    Styled the header and customized in the colors in the tailwind config file

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,26 @@
-import logo from "./logo.png";
+import logo from "./logo.svg";
 import "./Header.css";
 
 const Header = () => {
   return (
-    <header>
-      <img src={logo} alt="DashDiary logo" className="logo" />
+    <header className="md:flex md:items-center md:justify-between">
+      <div className="min-w-0 flex-1">
+        <img src={logo} alt="DashDiary logo" className="logo" />
+      </div>
+      <div className="mt-4 flex md:ml-4 md:mt-0">
+        <button
+          type="button"
+          className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-body shadow-sm ring-1 ring-inset ring-gray hover:bg-hovWhite"
+        >
+          Log a Race
+        </button>
+        <button
+          type="button"
+          className="ml-3 inline-flex items-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-hovPrimary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Profile
+        </button>
+      </div>
     </header>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.js"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#F6F1EE",
+      body: "#272121",
+      gray: "#d4d3d3",
+      primary: "#C63D2F",
+      secondary: "#FFBB5C",
+      darkOrange: "#E25E3E",
+      lightOrange: "#FF9B50",
+      hovWhite: "#f8f4f1",
+      hovPrimary: "#9e3126",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
